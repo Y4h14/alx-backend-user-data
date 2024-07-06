3382af2b622bd1b7dbadfafc8dca24d74dab3b24
Y4h14 <yahyasadig2013@gmail.com>
2024-07-06 18:13:29 +0300

    updated annotation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,7 +3,7 @@
 import logging
 import re
 import mysql.connector
-from typing import List, Collection
+from typing import List
 from os import getenv
 
 
@@ -68,8 +68,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db() -> Collection[mysql.connector.connection.MySQLConnection,
-                           None]:
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """connects to a database and return a curouser"""
     user = getenv('PERSONAL_DATA_DB_USERNAME', 'root')
     password = getenv('PERSONAL_DATA_DB_PASSWORD', '')
