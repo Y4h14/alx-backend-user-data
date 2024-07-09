b66e55cd7554465020e19dd6b3969a42ec5557f5
Y4h14 <yahyasadig2013@gmail.com>
2024-07-09 22:59:40 +0300

    added task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """define basic authentication"""
 from .auth import Auth
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -24,5 +26,28 @@ class BasicAuth(Auth):
         if authorization_header.startswith('Basic '):
             result = authorization_header.split('Basic ', 1)
             return result[1]
+        else:
+            return None
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) -> str:
+        """_summary_
+
+        Args:
+            base64_authorization_header (str): _description_
+
+        Returns:
+            str: return the decoded value of base64 string
+        """
+        if base64_authorization_header:
+            if isinstance(base64_authorization_header, str):
+                try:
+                    res = base64.b64decode(
+                        base64_authorization_header,
+                        validate=True
+                    )
+                    return res.decode('utf-8')
+                except (binascii.Error, UnicodeDecodeError):
+                    return None
         else:
             return None
