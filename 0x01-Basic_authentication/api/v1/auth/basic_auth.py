921a3e9883ade7dc32505bb016035962574031a7
Y4h14 <yahyasadig2013@gmail.com>
2024-07-09 22:48:51 +0300

    added task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,24 @@ from .auth import Auth
 
 class BasicAuth(Auth):
     """class for Basic Authentication"""
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """_summary_
+
+        Args:
+            authorization_header (str): _description_
+
+        Returns:
+            str: retrun Base64 part of the Authentication header
+        """
+        if authorization_header is None:
+            return None
+
+        if not isinstance(authorization_header, str):
+            return None
+
+        if authorization_header.startswith('Basic '):
+            result = authorization_header.split('Basic ', 1)
+            return result[1]
+        else:
+            return None
