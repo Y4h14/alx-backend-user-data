0d369104e392bef65ac6fd1849ada006884f0c08
Y4h14 <yahyasadig2013@gmail.com>
2024-07-11 10:32:22 +0300

    updated sessions in app

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -15,13 +15,13 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-auth = getenv('AUTH_TYPE', 'auth')
-if auth == 'basic_auth':
+auth_type = getenv('AUTH_TYPE', 'auth')
+if auth_type == 'basic_auth':
     auth = BasicAuth()
-elif auth == 'SessionAuth':
+if auth_type == 'session_auth':
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
-elif auth:
+if auth_type == 'auth':
     auth = Auth()
 
 
--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -6,13 +6,15 @@ from os import getenv
 from models.user import User
 
 
-@app_views.route('/auth_session/login', mehtods=['Post'],
-                 strict_slashed=False)
+@app_views.route('/auth_session/login', methods=['Post'],
+                 strict_slashes=False)
 def session_login():
     """handle login"""
-    email, password = request.form.get()
+    email = request.form.get('email')
     if not email:
         return jsonify({"error": "email missing"}), 400
+
+    password = request.form.get('password')
     if not password:
         return jsonify({"error": "password missing"}), 400
 
@@ -28,7 +30,7 @@ def session_login():
     if users[0].is_valid_password(password):
         from api.v1.app import auth
         session_id = auth.create_session(getattr(users[0], 'id'))
-        res = jsonify(users[0].to_json)
+        res = jsonify(users[0].to_json())
         res.set_cookie(getenv('SESSION_NAME'), session_id)
         return res
     return jsonify({"error": "wrong password"}), 401
