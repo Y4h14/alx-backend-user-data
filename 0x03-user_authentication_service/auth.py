2c5b4c298802058c23a5b6f21f1ca1e763296418
y4h14 <yahyasadig2013@gmail.com>
2024-08-24 18:30:49 +0300

    updated auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -21,8 +21,10 @@ class Auth:
     def register_user(self, email: str, password: str) -> User:
         """Register a new user into the system"""
 
-        if self._db.find_user_by(email=email):
-            raise ValueError(f'User {email} already exitsts')
-        hashed_pass = _hash_password(password=password)
-        user = self._db.add_user(email=email, password=hashed_pass)
-        return user
+        try:
+            self._db.find_user_by(email=email)
+        except Exception:
+            hashed_pass = _hash_password(password=password)
+            user = self._db.add_user(email=email, hashed_password=hashed_pass)
+            return user
+        raise ValueError(f'User {email} already exitsts')
