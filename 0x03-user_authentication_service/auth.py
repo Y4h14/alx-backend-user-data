a662c966de530bde287f7f8f05c242a8c1a01ff5
y4h14 <yahyasadig2013@gmail.com>
2024-08-24 19:31:10 +0300

    added task 8

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -28,3 +28,14 @@ class Auth:
             user = self._db.add_user(email=email, hashed_password=hashed_pass)
             return user
         raise ValueError(f'User {email} already exitsts')
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """locates a user by email and verify the password"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                if user.hashed_password == _hash_password(password):
+                    return True
+            return False
+        except Exception:
+            return False
